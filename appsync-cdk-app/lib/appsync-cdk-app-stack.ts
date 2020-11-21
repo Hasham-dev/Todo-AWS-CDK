9d14fdec1060328e572df368153f99e3a4ebe337
Hasham Vakani <hashamvakani@users.noreply.github.com>
2020-11-22 04:25:23 +0500

    adding deployment features

--- appsync-cdk-app/lib/appsync-cdk-app-stack.ts
+++ appsync-cdk-app/lib/appsync-cdk-app-stack.ts
@@ -2,13 +2,28 @@ import * as cdk from '@aws-cdk/core';
 import * as appsync from '@aws-cdk/aws-appsync';
 import * as ddb from '@aws-cdk/aws-dynamodb';
 import * as lambda from '@aws-cdk/aws-lambda';
-
+import * as s3 from "@aws-cdk/aws-s3";
+import * as origins from "@aws-cdk/aws-cloudfront-origins";
+import * as s3Deployment from "@aws-cdk/aws-s3-deployment";
+import * as cloudfront from "@aws-cdk/aws-cloudfront";
 
 export class AppsyncCdkAppStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-
+    const bucket = new s3.Bucket(this, "S3Bucket", {
+      publicReadAccess: true,
+      websiteIndexDocument: "index.html",
+    });
+    //s3 bucket deployment and specifying that where is the content
+    new s3Deployment.BucketDeployment(this, "buketdeploy", {
+      sources: [s3Deployment.Source.asset("../public")],
+      destinationBucket: bucket,
+    });
+    //cloudfront (aws cdn)
+    new cloudfront.Distribution(this, "distribution", {
+      defaultBehavior: { origin: new origins.S3Origin(bucket) },
+    });
     // Creates the AppSync API
     const api = new appsync.GraphqlApi(this, 'Api', {
       name: 'cdk-notes-appsync-api',
